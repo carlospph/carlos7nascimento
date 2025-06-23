6238b66ae975b7da972fd607f9e229f77473297a
carlospph <intablete@gmail.com>
2025-06-23 18:37:28 -0300

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
  import './App.css'
+import { Header } from './Components/Header/Header'
 
 function App() {
  
   return (
     <>
-       <h1>
-         Carlos Nascimento
-       </h1>
-        
+      <Header/>
     </>
   )
 }
